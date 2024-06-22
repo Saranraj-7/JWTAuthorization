b96b05d5cae4310e1a782aebdf4d41149038e618
SaranGuna7 <saranrajguna2001@gmail.com>
2024-06-22 12:10:55 +0530

    JWT Token

---
+++ src/main/java/com/example/demo/Service/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found !!!"));
+    }
+}
